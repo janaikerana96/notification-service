1538ac0d6d6e1bb15b1ae6b001db9a4be50f697d
janaikerana96 <janaikerana@gmail.com>
2022-12-15 22:15:19 +0100

    prisma config & dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationsDto } from './create-notification.dto.';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany()
+  }
+
+  @Post() 
+  async create(@Body() dto: CreateNotificationsDto) {
+    const { recipientId, content, category} = dto;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId
+      }
+    })    
   }
 }
---
+++ src/create-notification.dto..ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationsDto  {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+  @IsNotEmpty()
+  @Length(5,248)
+  content: string;
+  @IsNotEmpty()
+  category: string;
+}
